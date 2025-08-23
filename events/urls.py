86bfeffd720936663f449dbbf3a46b2aa93cea6a
RayhanKabir29 <rayhan.kabir29@gmail.com>
2025-08-23 22:10:01 +0600

    set header menu

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from events.views import show_events    
+from events.views import show_events,show_categories,show_participants
 
 urlpatterns = [
-    path('',show_events, name='events'),
+    path('', show_events, name='events'),                
+    path('categories/', show_categories, name='category'),  
+    path('participants/', show_participants, name='participant'), 
 ]
--- events/views.py
+++ events/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from events.forms import CategoryForm
 
 # Create your views here.
 
 def show_events(request):
-    return render(request, 'show_events.html')
-    
+    return render(request, "show_events.html")
+
+def show_categories(request):
+    return render(request, "show_categories.html")
+
+def show_participants(request):
+    return render(request, "show_participants.html")
+def crete_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Category Created Successfully")
+    return render(request, 'category.html', {'form': form})

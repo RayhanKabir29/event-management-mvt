06ce6367a37faccfee11ddecec9dcb342893d6b0
RayhanKabir29 <rayhan.kabir29@gmail.com>
2025-08-23 17:18:51 +0600

    make models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title   
+class Participant(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    participant_events = models.ManyToManyField(Event, related_name='participants', blank=True)
+
+    def __str__(self):
+        return self.name 

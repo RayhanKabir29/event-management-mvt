e7f7d039b07ab977e016fe356ebcbdf96afb8c88
RayhanKabir29 <rayhan.kabir29@gmail.com>
2025-08-25 21:54:14 +0600

    define home page

--- event_management/urls.py
+++ event_management/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from events.views import show_events
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", show_events, name="home"),
     path('events/',include('events.urls')),
 ]
  

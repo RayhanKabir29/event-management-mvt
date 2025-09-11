f32ab02e0e25fb9087165e8c4c497eeddc88ab7a
RayhanKabir29 <rayhan.kabir29@gmail.com>
2025-09-12 00:23:35 +0600

    update

--- users/views.py
+++ users/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def sign_up(request):
-    return render (request, 'registration/register.html')
+    if request.method == 'GET':
+        form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    return render (request, 'registration/register.html',{"form":form})
